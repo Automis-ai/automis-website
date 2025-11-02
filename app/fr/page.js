0746a87303b95ba8afa59404a1d9815a3f4f8ccd
Automis Admin <admin@automis.ai>
2025-11-02 16:03:32 +0000

    singular fix

--- app/fr/page.js
+++ app/fr/page.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import AkpagerLayout from "@/layouts/AkpagerLayout";
 import HeroSection from "./sections/HeroSection";
 import PainPointsSection from "./sections/PainPointsSection";
-import StepsSection from "./sections/StepsSection";
+import StepsSection from "./sections/StepSection";
 import ResultsSection from "./sections/ResultsSection";
 import AdvancedTechSection from "./sections/AdvancedTechSection";
 import BeforeAfterSection from "./sections/BeforeAfterSection";
