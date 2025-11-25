d78c86a1317e8b3859cffdf0de814acb8d03c572
Automis Admin <admin@automis.ai>
2025-11-25 18:02:34 +0000

    layout fix

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={${montserrat.variable} ${openSans.variable} bg-[#000a14]}
+        className={`${montserrat.variable} ${openSans.variable} bg-[#000a14]`}
         style={{ color: "white" }}
       >
         <Preloader />
@@ -46,18 +46,18 @@ export default function RootLayout({ children }) {
 
         {/* Microsoft Clarity */}
         <Script id="clarity" strategy="afterInteractive">
-          {
+          {`
             (function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "r97o757a1j");
-          }
+          `}
         </Script>
 
         {/* Facebook Pixel */}
         <Script id="facebook-pixel" strategy="afterInteractive">
-          {
+          {`
             !function(f,b,e,v,n,t,s){
               if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;
@@ -67,7 +67,7 @@ export default function RootLayout({ children }) {
             }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
             fbq('init','3659071671051477');
             fbq('track','PageView');
-          }
+          `}
         </Script>
 
         <noscript>
