f1d97949f23b6be84a9be8246107f2d396e973de
Automis Admin <admin@automis.ai>
2026-01-16 14:23:07 +0000

    GTM

--- app/layout.js
+++ app/layout.js
@@ -38,53 +38,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${montserrat.variable} ${openSans.variable} bg-[#000a14]`}
-        style={{ color: "white" }}
-      >
-        <Preloader />
-        <LocaleBootstrapper />
-        {children}
+   className={`${montserrat.variable} ${openSans.variable} bg-[#000a14]`}
+  style={{ color: "white" }}
+>
+  {/* Google Tag Manager */}
+  <Script
+    id="gtm-base"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MKNX6CK9');
+      `,
+    }}
+  />
 
-{/* Microsoft Clarity */}
-<Script id="microsoft-clarity" strategy="afterInteractive">
-  {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "r97o757a1j");
-  `}
-</Script>
+  <Preloader />
+  <LocaleBootstrapper />
+  {children}
 
-        {/* Facebook Pixel */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s){
-              if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;
-              n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];
-              t=b.createElement(e);t.async=!0;t.src=v;
-              s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)
-            }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init','3659071671051477');
-            fbq('track','PageView');
-          `}
-        </Script>
-
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=3659071671051477&ev=PageView&noscript=1"
-            alt="fb-pixel"
-          />
-        </noscript>
-
-        {/* LeadConnector Chatbot */}
-        <ChatWidgets />
-      </body>
-    </html>
-  );
-}
+  {/* LeadConnector Chatbot */}
+  <ChatWidgets />
+</body>
