ed5b6f954f67c80e8e9c317a25c551a84a7d3f17
Automis Admin <admin@automis.ai>
2026-01-16 13:33:09 +0000

    Microsoft clarity fix

--- app/layout.js
+++ app/layout.js
@@ -47,15 +47,13 @@ export default function RootLayout({ children }) {
         {children}
 
         {/* Microsoft Clarity */}
-        <Script id="clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "r97o757a1j");
-          `}
-        </Script>
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "r97o757a1j");
+</script>
 
         {/* Facebook Pixel */}
         <Script id="facebook-pixel" strategy="afterInteractive">
