8df5fba9cd55d87768d3a6e86f121e2abc37fd19
Automis Admin <admin@automis.ai>
2026-01-16 13:38:35 +0000

    microsoft clarity fix

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ import "@css/style.css";
 import { Montserrat, Open_Sans } from "next/font/google";
 import Script from "next/script";
 import ChatWidgets from "@/components/ChatWidgets";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   weight: ["700"],
@@ -46,14 +47,16 @@ export default function RootLayout({ children }) {
         <LocaleBootstrapper />
         {children}
 
-        {/* Microsoft Clarity */}
-        <script type="text/javascript">
+{/* Microsoft Clarity */}
+<Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "r97o757a1j");
-</script>
+  `}
+</Script>
 
         {/* Facebook Pixel */}
         <Script id="facebook-pixel" strategy="afterInteractive">
